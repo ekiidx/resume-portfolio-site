2b38217ebf65b20700c61529f5b69a2169dc1b09
ekiidx <info@vuedesign.co>
2024-03-05 23:59:25 -0500

    added stylesheet cachebuster

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,7 @@ if (isset($_SESSION["theme_color"])) {
     <meta property="og:image:height" content="630">
 
     <!-- STYLESHEETS -->
+    <?php $phpVersion = '?v=1.0.0'; ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!--<link rel="stylesheet" href="css/simple-line-icons.css" type="text/css" media="all">
     <link rel="stylesheet" href="css/slick.css" type="text/css" media="all">-->
