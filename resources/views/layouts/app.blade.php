7c454f512ff0e3389d975f8815362edc96311197
ekiidx <info@vuedesign.co>
2024-03-06 17:42:28 -0500

    fixed bottom footer layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -208,7 +208,9 @@ if (isset($_SESSION["theme_color"])) {
     </div>
 
     <footer class="footer-bottom">
-        <span class="copyright">© <?php echo date('Y'); ?> Erik Todd</span>
+        <div class="container">
+            <span class="copyright">© <?php echo date('Y'); ?> Erik Todd</span>
+        </div>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
