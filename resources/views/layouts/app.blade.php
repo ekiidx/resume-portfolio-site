1afdc653dd35bde8314718032b5d86b4aab01712
ekiidx <info@vuedesign.co>
2024-03-19 00:06:16 -0400

    removed col structure from main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,131 +86,123 @@ if (isset($_SESSION["theme_color"])) {
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-3 sidebar">
+    <div class="sidebar">
 
-                <header>
-                    <div class="name-title">
-                        <a href="/"><img src="/assets/img/erik-todd.jpg" alt="" class="circle profile-photo"></a>
-                        <a href="/"><span class="profile-name text-uppercase">Erik Todd</span></a>
-                    </div>
-                    <div class="nav-container">
-                        {{-- <nav>
-                            <ul role="tablist">
-                                <li class="text-uppercase"><a href="/#home">Home</a></li>
-                                <li class="text-uppercase"><a href="/#skills">Skills</a></li>
-                                <li class="text-uppercase"><a href="/#experience">Experience</a></li>
-                                <li class="text-uppercase"><a href="/#projects">Projects</a></li>
-                                <li class="text-uppercase"><a href="/#contact">Contact</a></li>
-                                <li class="text-uppercase"><a href="#">Resume</a></li>
-                            </ul>
-                        </nav> --}}
+            <header>
+                <div class="name-title">
+                    <a href="/"><img src="/assets/img/erik-todd.jpg" alt="" class="circle profile-photo"></a>
+                    <a href="/"><span class="profile-name text-uppercase">Erik Todd</span></a>
+                </div>
+                <div class="nav-container">
+                    {{-- <nav>
+                        <ul role="tablist">
+                            <li class="text-uppercase"><a href="/#home">Home</a></li>
+                            <li class="text-uppercase"><a href="/#skills">Skills</a></li>
+                            <li class="text-uppercase"><a href="/#experience">Experience</a></li>
+                            <li class="text-uppercase"><a href="/#projects">Projects</a></li>
+                            <li class="text-uppercase"><a href="/#contact">Contact</a></li>
+                            <li class="text-uppercase"><a href="#">Resume</a></li>
+                        </ul>
+                    </nav> --}}
 
-                        <nav class="navbar navbar-expand-lg" aria-label="Offcanvas navbar large">
-                        
-                            <a class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2">
-                                <span class="navbar-toggler-icon"></span>
-                            </a>
+                    <nav class="navbar navbar-expand-lg" aria-label="Offcanvas navbar large">
+                    
+                        <a class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2">
+                            <span class="navbar-toggler-icon"></span>
+                        </a>
 
-                            <!-- Off Canvas -->
-                            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
-                                <div class="offcanvas-header">
-                                {{-- <h5 class="offcanvas-title" id="offcanvasNavbar2Label">Offcanvas</h5> --}}
-                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                                </div>
-
-                                <div class="offcanvas-body">
-                                <ul id="navbar" class="navbar-nav" role="tablist">
-
-                                    <li class="nav-item"><a href="/#home"><button class="text-uppercase" data-bs-dismiss="offcanvas">Home</button></a></li>
-                                    <li class="nav-item text-uppercase"><a href="/#skills"><button class="text-uppercase" data-bs-dismiss="offcanvas">Skills</button></a></li>
-                                    <li class="nav-item text-uppercase"><a data-bs-dismiss="offcanvas" href="/#experience"><button class="text-uppercase" data-bs-dismiss="offcanvas">Experience</button></a></li>
-                                    <li class="nav-item text-uppercase"><a data-bs-dismiss="offcanvas" href="/#projects"><button class="text-uppercase" data-bs-dismiss="offcanvas">Projects</button></a></li>
-                                    <li class="nav-item text-uppercase"><a data-bs-dismiss="offcanvas" href="/#contact"><button class="text-uppercase" data-bs-dismiss="offcanvas">Contact</button></a></li>
-                                    <li class="nav-item text-uppercase"><a href="#">Resume</a></li>
-                                    {{-- <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Dropdown
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="#">Action</a></li>
-                                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                                        <li>
-                                        <hr class="dropdown-divider">
-                                        </li>
-                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                    </ul>
-                                    </li> --}}
-                                </ul>
-                                </div>
+                        <!-- Off Canvas -->
+                        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
+                            <div class="offcanvas-header">
+                            {{-- <h5 class="offcanvas-title" id="offcanvasNavbar2Label">Offcanvas</h5> --}}
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                             </div>
-                    
-                        </nav>
-                    </div>
 
-                        <footer class="footer-top social-icons">
-                
-                            <ul>
-                                <li class="social-icon">
-                                    <a href="https://linkedin.com/in/erik-todd">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path class="fill" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg></a></li>
-                    
-                                <li class="social-icon">
-                                    <a href="https://github.com/ekiidx">
-                    
-                                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 98 96" style="enable-background:new 0 0 98 96;" xml:space="preserve">
-                                            <style type="text/css">
-                                                .st0{fill-rule:evenodd;clip-rule:evenodd;}
-                                            </style>
-                                            <path class="st0" d="M48.9,0C21.8,0,0,22,0,49.2C0,71,14,89.4,33.4,95.9c2.4,0.5,3.3-1.1,3.3-2.4c0-1.1-0.1-5.1-0.1-9.1
-                                                c-13.6,2.9-16.4-5.9-16.4-5.9c-2.2-5.7-5.4-7.2-5.4-7.2c-4.4-3,0.3-3,0.3-3c4.9,0.3,7.5,5.1,7.5,5.1c4.4,7.5,11.4,5.4,14.2,4.1
-                                                c0.4-3.2,1.7-5.4,3.1-6.6c-10.8-1.1-22.2-5.4-22.2-24.3c0-5.4,1.9-9.8,5-13.2c-0.5-1.2-2.2-6.3,0.5-13c0,0,4.1-1.3,13.4,5.1
-                                                c4-1.1,8.1-1.6,12.2-1.6c4.1,0,8.3,0.6,12.2,1.6c9.3-6.4,13.4-5.1,13.4-5.1c2.7,6.8,1,11.8,0.5,13c3.2,3.4,5,7.8,5,13.2
-                                                c0,18.9-11.4,23.1-22.3,24.3c1.8,1.5,3.3,4.5,3.3,9.1c0,6.6-0.1,11.9-0.1,13.5c0,1.3,0.9,2.9,3.3,2.4C83.6,89.4,97.6,71,97.6,49.2
-                                                C97.7,22,75.8,0,48.9,0z"/>
-                                        </svg>
-                                    </a>
-                                </li>
-                            </ul>
-                    
-                    
-                    
-                            <span class="copyright">© <?php echo date('Y'); ?> Erik Todd</span>
-                        </footer>
+                            <div class="offcanvas-body">
+                            <ul id="navbar" class="navbar-nav" role="tablist">
 
-                        <div class="theme-selector theme-selector-mobile">
-                            <ul>
-                                <li class="light"><a href="?bg=light"></a></li>
-                                <li class="dark"><a href="?bg=dark"></a></li>
-                                <li class="orange"><a href="?bg=orange"></a></li>
-                                <li class="green"><a href="?bg=green"></a></li>
+                                <li class="nav-item"><a href="/#home"><button class="text-uppercase" data-bs-dismiss="offcanvas">Home</button></a></li>
+                                <li class="nav-item text-uppercase"><a href="/#skills"><button class="text-uppercase" data-bs-dismiss="offcanvas">Skills</button></a></li>
+                                <li class="nav-item text-uppercase"><a data-bs-dismiss="offcanvas" href="/#experience"><button class="text-uppercase" data-bs-dismiss="offcanvas">Experience</button></a></li>
+                                <li class="nav-item text-uppercase"><a data-bs-dismiss="offcanvas" href="/#projects"><button class="text-uppercase" data-bs-dismiss="offcanvas">Projects</button></a></li>
+                                <li class="nav-item text-uppercase"><a data-bs-dismiss="offcanvas" href="/#contact"><button class="text-uppercase" data-bs-dismiss="offcanvas">Contact</button></a></li>
+                                <li class="nav-item text-uppercase"><a href="#">Resume</a></li>
+                                {{-- <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li>
+                                    <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                                </li> --}}
                             </ul>
+                            </div>
                         </div>
-
-                </header>
-            </div>
-
-            <div class="main col-lg-9" data-bs-spy="scroll" data-bs-target="#navbar" data-bs-smooth-scroll="true" tabindex="0">
-
-                <div class="theme-selector theme-selector-desktop">
-                    <ul>
-                        <li class="light"><a href="?bg=light"></a></li>
-                        <li class="dark"><a href="?bg=dark"></a></li>
-                        <li class="orange"><a href="?bg=orange"></a></li>
-                        <li class="green"><a href="?bg=green"></a></li>
-                    </ul>
+                
+                    </nav>
                 </div>
 
-                @yield('content')
+                    <footer class="footer-top social-icons">
+            
+                        <ul>
+                            <li class="social-icon">
+                                <a href="https://linkedin.com/in/erik-todd">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path class="fill" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg></a></li>
+                
+                            <li class="social-icon">
+                                <a href="https://github.com/ekiidx">
+                
+                                    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 98 96" style="enable-background:new 0 0 98 96;" xml:space="preserve">
+                                        <style type="text/css">
+                                            .st0{fill-rule:evenodd;clip-rule:evenodd;}
+                                        </style>
+                                        <path class="st0" d="M48.9,0C21.8,0,0,22,0,49.2C0,71,14,89.4,33.4,95.9c2.4,0.5,3.3-1.1,3.3-2.4c0-1.1-0.1-5.1-0.1-9.1
+                                            c-13.6,2.9-16.4-5.9-16.4-5.9c-2.2-5.7-5.4-7.2-5.4-7.2c-4.4-3,0.3-3,0.3-3c4.9,0.3,7.5,5.1,7.5,5.1c4.4,7.5,11.4,5.4,14.2,4.1
+                                            c0.4-3.2,1.7-5.4,3.1-6.6c-10.8-1.1-22.2-5.4-22.2-24.3c0-5.4,1.9-9.8,5-13.2c-0.5-1.2-2.2-6.3,0.5-13c0,0,4.1-1.3,13.4,5.1
+                                            c4-1.1,8.1-1.6,12.2-1.6c4.1,0,8.3,0.6,12.2,1.6c9.3-6.4,13.4-5.1,13.4-5.1c2.7,6.8,1,11.8,0.5,13c3.2,3.4,5,7.8,5,13.2
+                                            c0,18.9-11.4,23.1-22.3,24.3c1.8,1.5,3.3,4.5,3.3,9.1c0,6.6-0.1,11.9-0.1,13.5c0,1.3,0.9,2.9,3.3,2.4C83.6,89.4,97.6,71,97.6,49.2
+                                            C97.7,22,75.8,0,48.9,0z"/>
+                                    </svg>
+                                </a>
+                            </li>
+                        </ul>
+
+                        <span class="copyright">© <?php echo date('Y'); ?> Erik Todd</span>
+                    </footer>
+
+                    <div class="theme-selector theme-selector-mobile">
+                        <ul>
+                            <li class="light"><a href="?bg=light"></a></li>
+                            <li class="dark"><a href="?bg=dark"></a></li>
+                            <li class="orange"><a href="?bg=orange"></a></li>
+                            <li class="green"><a href="?bg=green"></a></li>
+                        </ul>
+                    </div>
+            </header>
+        </div>
+
+        <div class="main" data-bs-spy="scroll" data-bs-target="#navbar" data-bs-smooth-scroll="true" tabindex="0">
+
+            <div class="theme-selector theme-selector-desktop">
+                <ul>
+                    <li class="light"><a href="?bg=light"></a></li>
+                    <li class="dark"><a href="?bg=dark"></a></li>
+                    <li class="orange"><a href="?bg=orange"></a></li>
+                    <li class="green"><a href="?bg=green"></a></li>
+                </ul>
             </div>
+
+            @yield('content')
         </div>
     </div>
 
     <footer class="footer-bottom">
-        <div class="container">
-            <span class="copyright">© <?php echo date('Y'); ?> Erik Todd</span>
-        </div>
+        <span class="copyright">© <?php echo date('Y'); ?> Erik Todd</span>
     </footer>
 
     <div id="btn-back-to-top" style="width: 2rem; max-width: 2rem; height: auto; cursor: pointer;">
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,48 +2,43 @@
 @section('title', 'Projects')
 
 @section('content')
-
     <section id="projects">
-        <div class="container">
+        <h2 class="text-uppercase">Projects</h2>
+        <p>{{ $projects->count() }} Projects</p>
+        
+        <table class="project-table">
+            <tr>
+                <th>Year</th>
+                <th></th>
+                <th>Name</th>
+                <th>Company</th>
+                <th>Tags</th>
+                <th>Link</th>
+            </tr>
 
-            <h2 class="text-uppercase">Projects</h2>
-            <p>{{ $projects->count() }} Projects</p>
-            
-            <table class="project-table">
+            @foreach ( $projects as $project )
                 <tr>
-                    <th>Year</th>
-                    <th></th>
-                    <th>Name</th>
-                    <th>Company</th>
-                    <th>Tags</th>
-                    <th>Link</th>
+                    <td>{{ $project->year }}</td>
+                    <td><a href="/projects/{{ $project->slug }}"><img style="max-width: 7rem;" src="/assets/img/{{ $project->img }}"></a></td>
+                    <td><a style="display: block; font-weight: 700;" href="/projects/{{ $project->slug }}">{{ $project->name }}</a></td>
+                    <td><p>{{ $project->company }}</p></td>
+                    <td><div>
+                        @foreach ($project->tags as $tag )
+                            <a class="btn circle-btn" href="/tags/{{ $tag->slug }}">{{ $tag->name }}</a>
+                        @endforeach
+                    </div></td>
+                    <td class="link"><a class="project-link" href="https://{{ $project->link }}">{{ $project->link }}
+                        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                        viewBox="0 0 32.5 32.5" class="link-arrow" xml:space="preserve">
+                        <g>
+                            <path class="link-arrow-path" d="M6.2,30.6c-1.5,1.5-3.2,2.9-5.1,1.1c-2.2-2-0.7-3.8,0.9-5.5c16-16,3.3-3.3,20.6-20.5C17.1,5.8,13.2,6,9.2,6.1
+                            c-1.9,0-3.8-0.3-3.9-2.8C5.2,0.4,7.2,0,9.3,0c6.3,0,12.7,0,19,0c3,0,4.2,1.3,4.2,4.2c-0.1,6.3,0,12.7,0,19c0,1.9-0.2,3.8-2.8,4
+                            c-2.8,0.2-3.3-1.8-3.2-4c0.1-3.9,0.3-7.9,0.6-13.5L6.2,30.6z"/>
+                        </g>
+                        </svg>
+                    </a></td>
                 </tr>
-
-                @foreach ( $projects as $project )
-                    <tr>
-                        <td>{{ $project->year }}</td>
-                        <td><a href="/projects/{{ $project->slug }}"><img style="max-width: 7rem;" src="/assets/img/{{ $project->img }}"></a></td>
-                        <td><a style="display: block; font-weight: 700;" href="/projects/{{ $project->slug }}">{{ $project->name }}</a></td>
-                        <td><p>{{ $project->company }}</p></td>
-                        <td><div>
-                            @foreach ($project->tags as $tag )
-                                <a class="btn circle-btn" href="/tags/{{ $tag->slug }}">{{ $tag->name }}</a>
-                            @endforeach
-                        </div></td>
-                        <td class="link"><a class="project-link" href="https://{{ $project->link }}">{{ $project->link }}
-                            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                            viewBox="0 0 32.5 32.5" class="link-arrow" xml:space="preserve">
-                            <g>
-                                <path class="link-arrow-path" d="M6.2,30.6c-1.5,1.5-3.2,2.9-5.1,1.1c-2.2-2-0.7-3.8,0.9-5.5c16-16,3.3-3.3,20.6-20.5C17.1,5.8,13.2,6,9.2,6.1
-                               c-1.9,0-3.8-0.3-3.9-2.8C5.2,0.4,7.2,0,9.3,0c6.3,0,12.7,0,19,0c3,0,4.2,1.3,4.2,4.2c-0.1,6.3,0,12.7,0,19c0,1.9-0.2,3.8-2.8,4
-                               c-2.8,0.2-3.3-1.8-3.2-4c0.1-3.9,0.3-7.9,0.6-13.5L6.2,30.6z"/>
-                            </g>
-                            </svg>
-                        </a></td>
-                    </tr>
-                @endforeach
-            </table>
-
-        </div>
+            @endforeach
+        </table>
     </section>
 @endsection
