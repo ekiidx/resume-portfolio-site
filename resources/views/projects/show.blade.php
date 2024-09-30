075ee9b43c6c5787193380a27a4bd2fe0e6c7a60
System Administrator <root@EKs-MacBook-Pro.attlocal.net>
2024-09-29 23:54:24 -0400

    added bullet_points column to projects table

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,7 +4,20 @@
 @section('content')
     <section class="projects project-show">
         <div class="container">
-            <h1 style="font-size: 3rem;">{{ $project->name }}</h1>
+            <a href="https://{{ $project->link }}">
+                <h1 style="font-size: calc(1.325rem + .9vw)">{{ $project->name }}</h1>
+            </a>
+                
+            <a class="project-link" href="https://{{ $project->link }}"><span>{{ $project->link }}</span>
+                <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox="0 0 32.5 32.5" class="link-arrow" xml:space="preserve">
+                <g>
+                    <path class="link-arrow-path" d="M6.2,30.6c-1.5,1.5-3.2,2.9-5.1,1.1c-2.2-2-0.7-3.8,0.9-5.5c16-16,3.3-3.3,20.6-20.5C17.1,5.8,13.2,6,9.2,6.1
+                    c-1.9,0-3.8-0.3-3.9-2.8C5.2,0.4,7.2,0,9.3,0c6.3,0,12.7,0,19,0c3,0,4.2,1.3,4.2,4.2c-0.1,6.3,0,12.7,0,19c0,1.9-0.2,3.8-2.8,4
+                    c-2.8,0.2-3.3-1.8-3.2-4c0.1-3.9,0.3-7.9,0.6-13.5L6.2,30.6z"/>
+                </g>
+                </svg>
+            </a>
             <p style="margin-bottom: 1rem;">{{ $project->description }}</p>
 
             <div style="margin-bottom: 1rem;">
@@ -13,7 +26,7 @@
                 @endforeach
             </div>
 
-            <img src="/assets/img/{{ $project->img }}"></a>
+            <a href="https://{{ $project->link}}"><img src="/assets/img/{{ $project->img }}"></a>
             <p style="white-space: pre-wrap;">{!! $project->content !!}</p>
             <a href="/projects" class="btn circle-btn">All Projects ({{ $all_projects->count() }})</a>
         </div>
