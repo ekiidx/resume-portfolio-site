7ec93443dcff446090418ec45a3a256c22031cb6
ekiidx <133534581+ekiidx@users.noreply.github.com>
2025-06-27 15:52:10 -0400

    added is_active column to projects table

--- database/migrations/2024_01_15_020551_create_projects_table.php
+++ database/migrations/2024_01_15_020551_create_projects_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->string('timeline');
             $table->text('content');
             $table->text('bullet_points');
+            $table->integer('is_active')->unsigned();
             $table->timestamps();
         });
     }
